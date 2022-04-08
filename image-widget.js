5173757df1417438a1643b4b284f6d576dc1aa41
TLTechSmart <85695580+TLTechSmart@users.noreply.github.com>
2022-04-08 10:54:52 +0100

    Update image-widget.js

--- image-widget.js
+++ image-widget.js
@@ -1,6 +1,9 @@
 let index;
+let imageLink;
 const imagesArr = require('image-widget.json');
+console.log("imagesArr:" , JSON.stringify(imagesArr);
 async function linkImage() {
     index = Math.floor(Math.random()*imagesArr.length);
-    return document.getElementById('widget_image').src = imagesArr[index].linkToImage;
+    imageLink = imagesArr[index].linkToImage;
+    return document.getElementById('widget_image').src = imageLink;
 }
