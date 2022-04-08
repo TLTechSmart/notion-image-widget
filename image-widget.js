6b940c1e29da2f8409f16b25a72b93b0a4f0182b
TLTechSmart <85695580+TLTechSmart@users.noreply.github.com>
2022-04-08 16:04:47 +0100

    Update image-widget.js

--- image-widget.js
+++ image-widget.js
@@ -3,5 +3,9 @@ async function linkImage() {
     let parsed = await response.json();
     let index = Math.floor(Math.random()*parsed.length);
     let imageSource = parsed[index].linkToImage;
+    let imageAlt = `Quote by ${parsed[index].source}. ${parsed[index].quote}`;
+    let imageTitle = `Quote by ${parsed[index].source}`;
     document.getElementById('widget_image').src = imageSource;
+    document.getElementById('widget_image').alt = imageAlt;
+    document.getElementById('widget_image').title = imageAlt;
 }
