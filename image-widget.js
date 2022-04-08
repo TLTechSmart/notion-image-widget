2dfbdea51c286a6f1dbbf70fdb695c3f0250e330
TLTechSmart <85695580+TLTechSmart@users.noreply.github.com>
2022-04-08 11:08:59 +0100

    Update image-widget.js

--- image-widget.js
+++ image-widget.js
@@ -1,9 +1,12 @@
 let index;
 let imageLink;
-const imagesArr = 'https://tltechsmart.github.io/notion-widgets/image_widget.json';
-console.log("imagesArr:" , JSON.stringify(imagesArr));
+import * as data from 'image_widget.json';
+const { linkToImage } = data;
+console.log(linkToImage);
+// const imagesArr = 'https://tltechsmart.github.io/notion-widgets/image_widget.json';
+// console.log("imagesArr:" , JSON.stringify(imagesArr));
 async function linkImage() {
-    index = Math.floor(Math.random()*imagesArr.length);
-    imageLink = imagesArr[index].linkToImage;
+    index = Math.floor(Math.random()*data.length);
+    imageLink = data[index].linkToImage;
     return document.getElementById('widget_image').src = imageLink;
 }
