7fcb832538a210d8c5b2f6335db8f98206304d24
TLTechSmart <85695580+TLTechSmart@users.noreply.github.com>
2022-04-11 16:32:58 +0100

    Update user-image-widget.js

--- user-image-widget.js
+++ user-image-widget.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 async function linkImage() {
     let response = await fetch("user-image-widget-list.json");
     let parsed = await response.json();
@@ -11,7 +13,12 @@ async function linkImage() {
 }
 
 async function addImage(imageName, imageLink) {
-    
+    let file = fs.readFileSync("user-image-widget-list.json", "utf-8");
+    let parsed = JSON.parse(file);
+    let blob = {'title': imageName, 'linkToImage': imageLink};
+    parsed.push(blob);
+    file = JSON.stringify(parsed);
+    fs.writeFileSync("user-image-widget-list.json", file, "utf-8");
     // let response = await fetch("user-image-widget-list.json", { method: "POST", body: blob });
     // console.log(response);
     // let parsed = await response.json();
@@ -21,8 +28,8 @@ async function addImage(imageName, imageLink) {
     // let imageTitle = `Quote by ${parsed[index].source}`;
     // document.getElementById('widget_image').src = imageSource;
     // document.getElementById('widget_image').alt = imageAlt;
-    let blob = {'title': imageName, 'linkToImage': imageLink};
-    console.log(blob);
+    // let blob = {'title': imageName, 'linkToImage': imageLink};
+    // console.log(blob);
     // parsed.push(blob);
     // console.log(parsed);
     // response = JSON.stringify(parsed);
@@ -33,5 +40,5 @@ async function addImage(imageName, imageLink) {
     // let data = new FormData();
     // data.append("upfile", new Blob([blob], {type: "text/plain"}));
     // fetch("user-image-widget-list.json", { method: "POST", body: data });
-    let response = await fetch("user-image-widget-list.json", { method: "POST", body: blob });
+    // let response = await fetch("user-image-widget-list.json", { method: "POST", body: blob });
 }
